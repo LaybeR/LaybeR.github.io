c7aee773636f71fb5b65f2477da2730b271b6f50
LaybeR <lennard.rizkallah@gmail.com>
2020-08-23 01:16:06 +0200

    Final design for now

--- questions.js
+++ questions.js
@@ -2,9 +2,9 @@ questions = [
     {
         "question": "Raptor tat nichts falsches!",
         "effect": {
-            "rBr": 10,
+            "rBr": -10,
             "aRp": 10,
-            "red": 10,
+            "red": -10,
             "gut": 10
         }
     }
