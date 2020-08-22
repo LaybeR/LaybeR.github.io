9fbd308b10e1eac8debe522e17715d1e5e6d7874
LaybeR <lennard.rizkallah@gmail.com>
2020-08-23 01:06:58 +0200

    Vielleicht jetzt

---
+++ questions.js
@@ -0,0 +1,11 @@
+questions = [
+    {
+        "question": "Raptor tat nichts falsches!",
+        "effect": {
+            "rBr": 10,
+            "aRp": 10,
+            "red": 10,
+            "gut": 10
+        }
+    }
+];
