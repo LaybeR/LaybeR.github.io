4ca96944eff82aa479cfb99cd7cde72abced1051
LaybeR <lennard.rizkallah@gmail.com>
2020-08-23 01:26:58 +0200

    Question Update

--- questions.js
+++ questions.js
@@ -2,9 +2,81 @@ questions = [
     {
         "question": "Raptor tat nichts falsches!",
         "effect": {
-            "rBr": -10,
-            "aRp": 10,
+            "rBr": 0,
+            "aRp": -10,
+            "red": 0,
+            "gut": -5
+        }
+    },
+    {
+        "question": "KANKE",
+        "effect": {
+            "rBr": 0,
+            "aRp": 0,
             "red": -10,
+            "gut": 5
+        }
+    },
+    {
+        "question": "Minsterfyo ist das aktivste Mitglied des MBundestag.",
+        "effect": {
+            "rBr": 0,
+            "aRp": 0,
+            "red": 10,
+            "gut": -5
+        }
+    },
+    {
+        "question": "Ifaq hat viel gutes getan.",
+        "effect": {
+            "rBr": -10,
+            "aRp": 0,
+            "red": 0,
+            "gut": 5
+        }
+    },
+    {
+        "question": "Was nicht festgeschrieben ist, ist nicht verboten.",
+        "effect": {
+            "rBr": 10,
+            "aRp": 0,
+            "red": 0,
+            "gut": -5
+        }
+    },
+    {
+        "question": "Ich würde die Piraten wählen",
+        "effect": {
+            "rBr": 0,
+            "aRp": 0,
+            "red": 10,
+            "gut": -5
+        }
+    },
+    {
+        "question": "Schiedsgerichte sollten Rechtswirksamer sein, als das Wort des Moderationsleiters.",
+        "effect": {
+            "rBr": -10,
+            "aRp": 0,
+            "red": 0,
+            "gut": 5
+        }
+    },
+    {
+        "question": "Der MBundestag ist sehr aktiv.",
+        "effect": {
+            "rBr": 0,
+            "aRp": 0,
+            "red": -10,
+            "gut": 5
+        }
+    },
+    {
+        "question": "Wenn morgen Bundestagswahl wäre wurde ich SMRP wählen.",
+        "effect": {
+            "rBr": 0,
+            "aRp": 0,
+            "red": 0,
             "gut": 10
         }
     }
