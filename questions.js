d95cdf954bb7228e8354b2e864bd056fc0ec2c41
LaybeR <42009151+LaybeR@users.noreply.github.com>
2020-08-23 00:35:18 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -7,5 +7,32 @@ questions = [
             "reddit": 10,
             "schlet": 10
         }
+    },
+    {
+        "question": "Raptor tat nichts falsches!",
+        "effect": {
+            "rBruch": 10,
+            "pRaptr": 10,
+            "reddit": 10,
+            "schlet": 10
+        }
+    },
+    {
+        "question": "Raptor tat nichts falsches!",
+        "effect": {
+            "rBruch": 10,
+            "pRaptr": 10,
+            "reddit": 10,
+            "schlet": 10
+        }
+    },
+    {
+        "question": "Raptor tat nichts falsches!",
+        "effect": {
+            "rBruch": 10,
+            "pRaptr": 10,
+            "reddit": 10,
+            "schlet": 10
+        }
     }
 ];
