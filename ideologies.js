c557e49a1aea07a9d8555b2d4ff7eadabf3d3cd6
LaybeR <lennard.rizkallah@gmail.com>
2020-08-23 00:43:51 +0200

    First Test

---
+++ ideologies.js
@@ -0,0 +1,101 @@
+ideologies = [
+    {
+        "name": "Raptorismus",
+        "stats": {
+            "rBruch": 100,
+            "pRaptr": 100,
+            "reddit": 60,
+            "schlet": 80
+        }
+    },
+    {
+        "name": "Sozialliberalismus",
+        "stats": {
+            "rBruch": 50,
+            "pRaptr": 50,
+            "reddit": 0,
+            "schlet": 70
+        }
+    },
+    {
+        "name": "Libertärer Ifaqismus",
+        "stats": {
+            "rBruch": 100,
+            "pRaptr": 10,
+            "reddit": 0,
+            "schlet": 50
+        }
+    },
+    {
+        "name": "Absoluter Nussismus",
+        "stats": {
+            "rBruch": 100,
+            "pRaptr": 0,
+            "reddit": 0,
+            "schlet": 0
+        }
+    },
+    {
+        "name": "Moderat extremer Nussismus",
+        "stats": {
+            "rBruch": 80,
+            "pRaptr": 20,
+            "reddit": 20,
+            "schlet": 20
+        }
+    },
+    {
+        "name": "Libhindismus",
+        "stats": {
+            "rBruch": 50,
+            "pRaptr": 50,
+            "reddit": 50,
+            "schlet": 50
+        }
+    },
+    {
+        "name": "Technokratischer Raumfahrtprogrammologismus",
+        "stats": {
+            "rBruch": 50,
+            "pRaptr": 100,
+            "reddit": 75,
+            "schlet": 75
+        }
+    },
+    {
+        "name": "Apfelmus",
+        "stats": {
+            "rBruch": 60,
+            "pRaptr": 20,
+            "reddit": 10,
+            "schlet": 20
+        }
+    },
+    {
+        "name": "Religiöser Semarcismus",
+        "stats": {
+            "rBruch": 0,
+            "pRaptr": 0,
+            "reddit": 0,
+            "schlet": 100
+        }
+    },
+    {
+        "name": "Darrinistischer Semarcismus",
+        "stats": {
+            "rBruch": 20,
+            "pRaptr": 10,
+            "reddit": 20,
+            "schlet": 100
+        }
+    },
+    {
+        "name": "Radikaler Antizentrismus",
+        "stats": {
+            "rBruch": 0,
+            "pRaptr": 0,
+            "reddit": 0,
+            "schlet": 0
+        }
+    },
+];
---
+++ questions.js
@@ -0,0 +1,38 @@
+questions = [
+    {
+        "question": "Raptor tat nichts falsches!",
+        "effect": {
+            "rBruch": 10,
+            "pRaptr": 10,
+            "reddit": 10,
+            "schlet": 10
+        }
+    },
+    {
+        "question": "Raptor tat nichts falsches!",
+        "effect": {
+            "rBruch": 10,
+            "pRaptr": 10,
+            "reddit": 10,
+            "schlet": 10
+        }
+    },
+    {
+        "question": "Raptor tat nichts falsches!",
+        "effect": {
+            "rBruch": 10,
+            "pRaptr": 10,
+            "reddit": 10,
+            "schlet": 10
+        }
+    },
+    {
+        "question": "Raptor tat nichts falsches!",
+        "effect": {
+            "rBruch": 10,
+            "pRaptr": 10,
+            "reddit": 10,
+            "schlet": 10
+        }
+    }
+];
